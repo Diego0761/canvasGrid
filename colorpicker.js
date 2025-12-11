39541ca986adeaff92b869b1eaf468b1af572703
Diego <didipinheiro@outlook.com>
2025-12-10 23:42:20 -0300

    fix: add newline for improved code readability in colorpicker.js

--- colorpicker.js
+++ colorpicker.js
@@ -3,6 +3,7 @@ const colors = document.querySelectorAll('.color')
 let selectedColor = localStorage.getItem('selectedColor') || '#000000'
 
 const selectedDiv = document.getElementById(selectedColor)
+
 selectedDiv.classList.add('selected-color')
 
 colors.forEach(colorDiv => {
